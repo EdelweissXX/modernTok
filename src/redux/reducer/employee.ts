42db0763c8c01ae81f4f7aa430459f33ef204e09
Tenlong <tlwork2021@163.com>
2022-05-20 07:08:11 +0800

    5/20

---
+++ src/redux/reducer/employee.ts
@@ -0,0 +1,19 @@
+
+
+export default function indexGetUser<T extends (state:unknown,action:{type:any,data:any})=>any>(state:unknown, action:{type:any,data:any}) {
+    const {type} = action
+    switch (type) {
+        case type.START_FETCH:
+            return {...state as {}, isFetching: true};
+
+        case  type.FINISH_FETCH:
+            return {...state as {}, isFetching: false};
+
+        case type.SET_ERROR:
+            return {...state as {}, error: action.data}
+
+        default:
+            return state;
+    }
+}
+
